803c9bafc55c3fb2c90dfe517b4c84042d454600
oafidi <omarafidi2005@gmail.com>
2025-04-24 13:22:55 +0100

    Update mutex.c

--- philo/helper_functions/mutex.c
+++ philo/helper_functions/mutex.c
@@ -12,53 +12,48 @@
 
 #include "../philosophers.h"
 
-void	destroy_mutex(t_dinning *dinner, int flag, int nbr)
+// Libère les mutex selon le drapeau passé
+void destroy_mutex(t_dinning *dinner, int flag, int nbr)
 {
-	int	i;
+    int i = 0;
 
-	i = 0;
-	if (15 == flag)
-	{
-		while (i < nbr)
-			pthread_mutex_destroy(&dinner->forks[i++]);
-		pthread_mutex_destroy(&dinner->print);
-		pthread_mutex_destroy(&dinner->meals);
-		pthread_mutex_destroy(&dinner->dead);
-	}
-	else if (6 == flag)
-	{
-		pthread_mutex_destroy(&dinner->print);
-		pthread_mutex_destroy(&dinner->meals);
-	}
-	else
-		pthread_mutex_destroy(&dinner->print);
+    if (15 == flag)
+    {
+        while (i < nbr)
+            pthread_mutex_destroy(&dinner->forks[i++]);
+        pthread_mutex_destroy(&dinner->print);
+        pthread_mutex_destroy(&dinner->meals);
+        pthread_mutex_destroy(&dinner->dead);
+    }
+    else if (6 == flag)
+    {
+        pthread_mutex_destroy(&dinner->print);
+        pthread_mutex_destroy(&dinner->meals);
+    }
+    else
+        pthread_mutex_destroy(&dinner->print);
 }
 
-int	init_mutex(t_dinning *dinner)
+// Initialise tous les mutex nécessaires (forks, impression, état...)
+int init_mutex(t_dinning *dinner)
 {
-	int	i;
+    int i = 0;
 
-	if (pthread_mutex_init(&dinner->print, NULL))
-		return (write(2, "Failed to inisialize print mutex !!\n", 37), 0);
-	if (pthread_mutex_init(&dinner->meals, NULL))
-	{
-		write(2, "Failed to inisialize meals mutex !!\n", 37);
-		return (destroy_mutex(dinner, PRINT, 0), 0);
-	}
-	if (pthread_mutex_init(&dinner->dead, NULL))
-	{
-		write(2, "Failed to inisialize dead mutex !!\n", 36);
-		return (destroy_mutex(dinner, PRINT | MEAL, 0), 0);
-	}
-	i = 0;
-	while (i < dinner->nbr_philos)
-	{
-		if (pthread_mutex_init(&dinner->forks[i], NULL))
-		{
-			write(2, "Failed to initialize a fork mutex !!\n", 38);
-			return (destroy_mutex(dinner, FORK | PRINT | MEAL | DEAD, i), 0);
-		}
-		i++;
-	}
-	return (1);
+    if (pthread_mutex_init(&dinner->print, NULL))
+        return (write(2, "Failed to inisialize print mutex !!\n", 37), 0);
+
+    if (pthread_mutex_init(&dinner->meals, NULL))
+        return (write(2, "Failed to inisialize meals mutex !!\n", 37), destroy_mutex(dinner, PRINT, 0), 0);
+
+    if (pthread_mutex_init(&dinner->dead, NULL))
+        return (write(2, "Failed to inisialize dead mutex !!\n", 36), destroy_mutex(dinner, PRINT | MEAL, 0), 0);
+
+    while (i < dinner->nbr_philos)
+    {
+        if (pthread_mutex_init(&dinner->forks[i], NULL))
+            return (write(2, "Failed to initialize a fork mutex !!\n", 38), destroy_mutex(dinner, FORK | PRINT | MEAL | DEAD, i), 0);
+        i++;
+    }
+
+    return (1);
 }
