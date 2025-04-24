f7c9aa39434a7657ef453e26332025090c394598
oafidi <omarafidi2005@gmail.com>
2025-04-24 13:20:08 +0100

    Update time.c

--- philo/helper_functions/time.c
+++ philo/helper_functions/time.c
@@ -12,23 +12,49 @@
 
 #include "../philosophers.h"
 
+/**
+ * get_time - Obtient le temps actuel en millisecondes depuis l'époque Unix.
+ * Cette fonction utilise `gettimeofday` pour récupérer le temps actuel avec
+ * une précision de millisecondes, en renvoyant le temps écoulé depuis le 1er janvier 1970
+ * (l'époque Unix).
+ * En cas d'erreur lors de la récupération du temps, un message d'erreur est affiché
+ * et la fonction retourne 0.
+ *
+ * Return: Le temps actuel en millisecondes, ou 0 en cas d'erreur.
+ */
 long	get_time(void)
 {
 	struct timeval	_time;
 
-	if (gettimeofday(&_time, NULL) == -1) // 9eleb 3liha
+	// Récupère l'heure actuelle en microsecondes
+	if (gettimeofday(&_time, NULL) == -1)
 	{
 		write(2, "Failed to get the time of day !!\n", 34);
-		return (0);
+		return (0); // Si l'appel échoue, retourne 0
 	}
+	// Convertit le temps en millisecondes (tv_sec est en secondes, tv_usec en microsecondes)
 	return (_time.tv_sec * 1000 + _time.tv_usec / 1000);
 }
 
+/**
+ * precise_usleep - Pause le programme pendant un nombre de millisecondes spécifié.
+ * Cette fonction suspend l'exécution du programme pendant le nombre de millisecondes
+ * spécifié par `msec`. La fonction s'assure également que le dîner n'a pas été
+ * interrompu en vérifiant régulièrement l'état de mort du dîner. Elle utilise
+ * `usleep` pour effectuer des pauses de 100 microsecondes, permettant un contrôle
+ * plus fin de l'attente.
+ *
+ * @msec: Le nombre de millisecondes pendant lesquelles la fonction doit dormir.
+ * @dinner: La structure représentant l'état du dîner, utilisée pour vérifier si
+ *          le dîner a été interrompu (par exemple, si un philosophe est mort).
+ */
 void	precise_usleep(long msec, t_dinning *dinner)
 {
 	long	start;
 
-	start = get_time();
-	while (get_time() - start < msec && !get_dead_flag(dinner))// usleep fiha wa7ed erreur dakchi 3lach kan7awlo nseghro meno 9leb kter 3liha
-		usleep(100);
+	start = get_time(); // Récupère l'heure de départ
+	// Boucle de sommeil jusqu'à ce que le temps écoulé dépasse msec ou jusqu'à la mort d'un philosophe
+	while (get_time() - start < msec && !get_dead_flag(dinner))
+		usleep(100); // Attendre par petites périodes de 100 microsecondes
 }
+
